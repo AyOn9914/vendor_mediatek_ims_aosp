4ef0ce0c7d4e9c72915146216596a099e88c6ec5
Zinadin Zidan <zidan44@pixelexperience.org>
2022-01-12 15:19:59 +0600

    mtk-ims: packages: Update EngineerMode app
    * From: ares-user 12 SP1A.210812.016 21.11.8 release-keys
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I04b4d8b13d84943400295a0f60ac6c9c59d95bc5

--- mtk-engi.mk
+++ mtk-engi.mk
@@ -2,7 +2,3 @@
 PRODUCT_PACKAGES += \
     EngineerMode \
     libem_support_jni
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-mediatek-engineer.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek-engineer.xml
