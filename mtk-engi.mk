866988f50f5eb57ebfa666de6f3508426d696d41
Zinadin Zidan <zidan44@pixelexperience.org>
2022-03-17 12:11:04 +0600

    mtk-ims: packages: Update MTK IMS prebuilds
    * Changelog:
           - Sync with latest aosp changes
           - Built on android-12.1.0_r1
    
    [Build date: 17/03/22]
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Icea85e5655684f5ac29dd1f8d80062abf1ef4fcf

--- mtk-engi.mk
+++ mtk-engi.mk
@@ -1,4 +1,10 @@
 # Apps
 PRODUCT_PACKAGES += \
-    EngineerMode \
-    libem_support_jni
+    EngineerMode
+
+# JNI Libs
+PRODUCT_PACKAGES += \
+    libem_audio_jni \
+    libem_support_jni \
+    libem_usb_jni \
+    libem_wifi_jni
